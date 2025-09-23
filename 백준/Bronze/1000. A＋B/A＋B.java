8648c0c2131500b2cc55d9ea3ff0f205a0f49ef2
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-09-23 16:40:38 +0900

    [Bronze V] Title: A+B, Time: 168 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner Scanner = new Scanner(System.in);
+        int a = Scanner.nextInt();
+        int b = Scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
