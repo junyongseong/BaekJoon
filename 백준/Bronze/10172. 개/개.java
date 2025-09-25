031cfe9ba446ef6f02164d122318c7d62df57aa9
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-09-25 21:01:13 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("|\\_/|");
+        bw.newLine();
+        bw.write("|q p|   /}");
+        bw.newLine();
+        bw.write("( 0 )\"\"\"\\");
+        bw.newLine();
+        bw.write("|\"^\"`    |");
+        bw.newLine();
+        bw.write("||_/=\\\\__|");
+        bw.newLine();
+
+        bw.flush();
+    }
+}
