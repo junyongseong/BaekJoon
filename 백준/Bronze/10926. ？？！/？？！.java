7bb254b24093e6bc30f29d9a658d3206b688c685
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-09-25 15:48:46 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        // 코드 작성
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        System.out.println(a+"??!");
+
+    }
+}
