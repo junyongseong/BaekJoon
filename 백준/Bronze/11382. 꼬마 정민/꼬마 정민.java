4faabaf9981b7c0a4b759cae203aff443ea9de60
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-09-25 20:00:25 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+
+        System.out.println(A+B+C);
+
+        sc.close();
+    }
+}
