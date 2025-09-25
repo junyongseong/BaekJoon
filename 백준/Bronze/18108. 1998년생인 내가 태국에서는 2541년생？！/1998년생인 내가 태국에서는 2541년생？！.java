aaa91cb4a02dabdbbd120608a32e6abb2c44f40f
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-09-25 16:00:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        // 코드 작성
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println(a-543);
+    }
+}
