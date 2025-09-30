ee90fdd5db87d2b02c02175c897ce3b9db0118b7
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-09-30 14:26:39 +0900

    [Bronze V] Title: A+B - 3, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+            sb.append("\n");
+        }
+        System.out.print(sb);
+
+    }
+}
