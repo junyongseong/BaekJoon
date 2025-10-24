f7046f8d770bfc7ede09baae367412d0e8f47449
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-10-24 10:24:01 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14260 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,14 +1,28 @@
 import java.io.*;
 
 class Main {
+    // public static void main(String[] args) throws IOException {
+    //     // 코드 작성
+    //     BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    //     int L = Integer.parseInt(br.readLine());
+    //     String str = br.readLine();
+    //     int result =0;
+    //     for(int i=0;i<L;i++){
+    //         result +=(str.charAt(i)-'a'+1)* Math.pow(31, i);
+    //     }
+    //     System.out.println(result);
+    // }
     public static void main(String[] args) throws IOException {
         // 코드 작성
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int result =0;
+        long result =0;
+        long r =1;
         for(int i=0;i<L;i++){
-            result +=(str.charAt(i)-'a'+1)* Math.pow(31, i);
+            int val =str.charAt(i)-'a'+1;
+            result = (result + val *r )%1234567891;
+            r= (r*31)%1234567891;
         }
         System.out.println(result);
     }
