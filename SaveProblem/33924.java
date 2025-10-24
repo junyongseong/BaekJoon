1873db7021abb4428b4a704cdb9b0316ce113099
junyongseong <seongyongjun1160@gmail.com>
2025-10-24 19:01:54 +0900

    신묘마루 기묘망치

--- SaveProblem/33924.java
+++ SaveProblem/33924.java
@@ -3,5 +3,12 @@ import java.io.IOException;
 class Main {
     public static void main(String[] args) throws IOException {
         // 코드 작성
+        /*
+         * 햄부기 찾기
+            8개 밥그릇 4행 2열 // 2차원 배열[4][2]
+            기술 A : 1행 1열 <-> 2행 2열, 2행 1열 <->1행 2열
+            3행 1열<-> 4행 2열, 3행 2열 <-> 4행 1열 <-> 3행 2열
+            기술 B : 1행 과 2행에 있는 밥그릇 4개
+         */
     }
 }
