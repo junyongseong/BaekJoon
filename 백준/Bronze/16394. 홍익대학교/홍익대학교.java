2c63d5bdcd5260a1c381c3a41270782d57bd9983
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-11-07 10:20:19 +0900

    [Bronze V] Title: 홍익대학교, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+
+        System.out.println(A-1946);
+    }
+}
