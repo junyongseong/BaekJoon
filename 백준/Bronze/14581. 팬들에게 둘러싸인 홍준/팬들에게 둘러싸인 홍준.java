828d13ca5a8f8db98e1e99080381320075179ef8
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-11-15 21:11:28 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 184 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        Scanner scan = new Scanner(System.in);
+        String str= scan.next();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+str+"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
